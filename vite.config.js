4662ad7178945f3fd4eafd7444c7838c87a86ba8
Adarshjeet0 <adarshjeetc@gmail.com>
2025-05-03 13:31:47 +0530

    h

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/e-commerce-frontend",
   plugins: [react()],
   resolve: {
     alias: {
